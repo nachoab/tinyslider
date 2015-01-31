b8afe2e9aa66024aeab382020ffc44c439326209
Nacho Garcia <nachoab@gmail.com>
2015-01-31 13:21:07 +0100

    probando observar colecciones

--- src/client/model.js
+++ src/client/model.js
@@ -8,11 +8,17 @@ var update = function(doc) {
   model.tvs = Tvs.find().fetch();
 
   Object.observe(model.tvs, function(changes) {
-    console.log(changes);
-    // Tvs.remove(changes)
-    // Tvs.insert()
+    var changes = changes[0];
+
+    if (changes.type === 'add') {
+      var key = Number(changes.name);
+      Tvs.insert(changes.object[key]);
+    } else if (changes.type === 'delete') {
+      Tvs.remove(changes.oldValue);
+    }
   });
 }
+
 Tvs.find().observe({
   added: update,
   changed: update,
